5375ec6d0b49a33f2d34f4ac38e1af5ed8e81621
Ulduz <ulduzagaeva5@gmail.com>
2024-03-20 23:12:41 +0400

    Initial commit

---
+++ app/src/main/java/com/homework/code8/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.homework.code8
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
